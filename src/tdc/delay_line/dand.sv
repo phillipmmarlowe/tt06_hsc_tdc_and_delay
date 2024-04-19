dbc564fba992312b80742ebbecef9bc411438f20
Phillip Marlowe <pmarlowe@ucsc.edu>
2024-04-18 17:14:29 -0700

    Added conb_1 cells to hold constant 1, hopefully solve placement error in TT flow

--- src/tdc/delay_line/dand.sv
+++ src/tdc/delay_line/dand.sv
@@ -7,21 +7,49 @@ Description: Guaranteed to be an and chain independent of inputs
 */
 `define AND_CELL sky130_fd_sc_hd__and2_1  
 `timescale 1ns/1ps
-module dand #(parameter WIDTH=32) ( 
-	input 			    in_i,
-	output [WIDTH-1:0]	ffout_o
-);
 
-	wire [WIDTH:0] ffout_w;
-	assign ffout_w[0] = in_i;
-    assign ffout_o = ffout_w[WIDTH:1];
+
+module const_ones #(parameter N=64) (
+    output [N-1:0] ones
+);
+    genvar i;
+    generate
+        for(i=0; i<N; i=i+1) begin : const_ones_genblk
+            sky130_fd_sc_hd__conb_1 const_one(
+                .HI(ones[i])
+                `ifdef USE_POWER_PINS
+                    , .VGND(VGND)
+                    , .VPWR(VPWR)
+                    , .VPB(VPWR)
+                    , .VNB(VGND)
+                `endif  // USE_POWER_PINS 
+            );
+        end
+    endgenerate
+endmodule
+
+
+module dand #(parameter WIDTH_p=32) ( 
+	input 			    pulse_i,
+	output [WIDTH_p-1:0]	meas_o
+);
+    
+	(* keep *) wire [WIDTH_p:0] ffout_w;
+	(* keep *) wire [WIDTH_p-1:0]
+	
+	const_ones #(.N(WIDTH)) ones(
+        .ones(a_int)
+    );
+	
+	assign ffout_w[0] = pulse_i;
+    assign ffout_o = ffout_w[WIDTH_p:1];
 	
 	generate 
 		genvar i;
-		for(i=0; i<WIDTH; i=i+1) begin : dand_genblk
+		for(i=0; i<WIDTH_p; i=i+1) begin : dand_genblk
 		    `AND_CELL DA ( 
                 .X(ffout_w[i+1]), 
-                .A(1'b1), 
+                .A(a_int), 
                 .B(ffout_w[i])
                 `ifdef USE_POWER_PINS
                     , .VGND(VGND)
